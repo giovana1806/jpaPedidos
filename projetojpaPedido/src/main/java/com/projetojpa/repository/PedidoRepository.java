09c5521373778e84b0a735a55e212efcb8f23431
giovana1806 <132360035+giovana1806@users.noreply.github.com>
2023-10-31 08:37:05 -0300

    Delete projetojpaPedido directory

--- projetojpaPedido/src/main/java/com/projetojpa/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projetojpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projetojpa.entities.Pedido;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long>{
-	
-}
